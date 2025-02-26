7dc89aa153a84828e5d079155df973f197060f5e
URIELLE N'ZEBO <nsyntyche@gmail.com>
2025-02-26 17:45:01 +0000

    function

---
+++ routes/file-load-content.go
@@ -0,0 +1,13 @@
+package routes
+
+import (
+  "context"
+  "encoding/json"
+  "net/http"
+  "services/core"
+  "services/logs"
+  "services/models"
+  "time"
+)
+func FileLoadContent(ctx context.Context, w http.ResponseWriter, r *http.Request){  
+}
